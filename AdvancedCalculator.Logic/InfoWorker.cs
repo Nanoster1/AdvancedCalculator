47b46525c288d710a460486497333ef1ca852de1
Небытов Даниил <danil2002.11@mail.ru>
2021-03-09 23:34:55 +0500

    Оптимизация

--- AdvancedCalculator.Logic/InfoWorker.cs
+++ AdvancedCalculator.Logic/InfoWorker.cs
@@ -7,29 +7,47 @@ namespace AdvancedCalculator.Logic
     public class InfoWorker
     {
         public List<Calculator> Calculators { get; set; }
+        public List<double> Range { get; private set; }
         public InfoWorker(string expression, string range, string step)
         {
             double dStep = double.Parse(step);
-            List<double> lRange = SetRange(range, dStep);
-            Calculators = SetCalcs(expression, lRange);
+            Range = SetRange(range, dStep);
+            Calculators = SetCalcs(expression);
         } 
-        List<Calculator> SetCalcs(string expression, List<double> lRange)
+        List<Calculator> SetCalcs(string expression)
         {
-                List<Calculator> calculators = new List<Calculator>();
-                for (int i = 0; i < lRange.Count; i++)
-                {
-                    Calculator calculator = new Calculator("⊥" + expression + "⊥", lRange[i].ToString());
-                    calculators.Add(calculator);
-                }
+            List<Calculator> calculators = new List<Calculator>();
+            RPN rpn = new RPN("⊥" + expression + "⊥");
+            int index = 0;
+            bool neg = false;
+            string oldX = "x";
+            if (rpn.Rpn.Contains('x'))
+            {
+                index = rpn.Rpn.IndexOf('x');
+            }
+            else 
+            {
+                index = rpn.Rpn.IndexOf("-x");
+                neg = true;
+                oldX = "-x";
+            }
+            for (int i = 0; i < Range.Count; i++)
+            {
+                Calculator calculator = new Calculator(rpn, Range[i], index, neg, oldX);
+                calculators.Add(calculator);
+                oldX = Range[i].ToString();
+            }
                 return calculators;
         }
         List<double> SetRange(string range, double step)
         {
             string[] info = range.Split("--");
             double doubleRange = double.Parse(info[1]) - double.Parse(info[0]) + 1;
-            string s = step.ToString();
-            string[] s2 = s.Split(",");
-            int afterq = step.ToString().Split(",")[1].Length;
+            int afterq = 0;
+            if (step.ToString().Contains(","))
+                afterq = step.ToString().Split(",")[1].Length;
+            else
+                afterq = 0;
             var ar = new List<double>();
             for (double i = 0; i < doubleRange; i += step)
             {
---
+++ AdvancedCalculator.Logic/RPN.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdvancedCalculator.Logic
+{
+    public class RPN
+    {
+        public List<object> Rpn { get; private set; }
+        public string RPNStr { get; private set; }
+        public RPN(string expression)
+        {
+            Rpn = GetRPN(expression).ToList();
+            RPNStr = GetRPNStr();
+        }
+        public object[] GetRPN(string text)
+        {
+            List<object> expression = ParseExpression(text);
+            return ParseInRPN(expression);
+        }
+        List<object> ParseExpression(string text)                                                              //Парсим выражение в лист объектов
+        {
+            List<object> expression = new List<object>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (CheckDigit(text[i]) || (text[i] == '-' && !CheckDigit(text[i - 1]) && text[i - 1] != ')' && text[i + 1] != 'x'))                                                            //Если число
+                    expression.Add(ReadNumber(text, ref i));
+                else if (CheckLeftBracket(text[i]) || CheckRightBracket(text[i]) || CheckStartEnd(text[i]))
+                    expression.Add(text[i].ToString());
+                else if (char.IsWhiteSpace(text[i]))
+                    continue;
+                else if (text[i] == 'x')
+                    expression.Add(text[i]);
+                else if (text[i] == '-' && text[i + 1] == 'x' && !CheckDigit(text[i - 1]) && text[i - 1] != 'x')
+                {
+                    expression.Add(text[i].ToString() + text[i + 1].ToString());
+                    i++;
+                }
+                else
+                    expression.Add(ReadOperation(text, ref i));
+            }
+            return expression;
+        }
+        double ReadNumber(string text, ref int i)
+        {
+            StringBuilder num = new StringBuilder().Append(text[i]);
+            while (CheckDigit(text[i + 1]) || text[i + 1] == ',')
+            {
+                i++;
+                num.Append(text[i]);
+            }
+            return double.Parse(num.ToString());
+        }
+        Operation ReadOperation(string text, ref int i)
+        {
+            StringBuilder @string = new StringBuilder().Append(text[i]);
+            if ("+-*/^".Contains(text[i]))
+                return ChooseOp(text[i].ToString());
+            else
+            {
+                while (!CheckDigit(text[i + 1]) && text[i + 1] != '(' && text[i + 1] != ')')
+                {
+                    i++;
+                    @string.Append(text[i]);
+                }
+                return ChooseOp(@string.ToString());
+            }
+        }
+        bool CheckDigit(char text)                       //Проверяем на число
+        {
+            if (double.TryParse(text.ToString(), out _))
+                return true;
+            return false;
+        }
+        bool CheckStartEnd(object text)                                //Проверяем на символ начала/конца выражения
+        {
+            if (text.ToString() == "⊥")
+                return true;
+            return false;
+        }
+        bool CheckLeftBracket(object text)
+        {
+            if (text.ToString() == "(")
+                return true;
+            return false;
+        }
+        bool CheckRightBracket(object text)
+        {
+            if (text.ToString() == ")")
+                return true;
+            return false;
+        }
+        public Operation ChooseOp(object sym)
+        {
+            Operation op = null;
+            switch (sym.ToString())
+            {
+                case ("+"):
+                    op = new Plus();
+                    break;
+                case ("-"):
+                    op = new Minus();
+                    break;
+                case ("*"):
+                    op = new Mult();
+                    break;
+                case ("/"):
+                    op = new Div();
+                    break;
+                case ("log"):
+                    op = new Log();
+                    break;
+                case ("sin"):
+                    op = new Sin();
+                    break;
+                case ("cos"):
+                    op = new Cos();
+                    break;
+                case ("tg"):
+                    op = new Tan();
+                    break;
+                case ("^"):
+                    op = new Rank();
+                    break;
+                case ("sqrt"):
+                    op = new Sqrt();
+                    break;
+            }
+            return op;
+        }
+        object[] ParseInRPN(List<object> expression)
+        {
+            Stack<object> california = new Stack<object>();
+            Stack<object> texas = new Stack<object>();
+            int i = 0;
+            while (true)
+            {
+                if (i == 0)                                                     //Символ начала строки идёт сразу во 2-й стек 
+                {
+                    texas.Push(expression[i]);
+                    i++;
+                }
+                else if (expression[i] is double || expression[i].ToString() == 'x'.ToString() || expression[i].ToString() == "-x")
+                {
+                    california.Push(expression[i]);
+                    i++;
+                }
+                else if (CheckLeftBracket(expression[i]))
+                {
+                    texas.Push(expression[i]);
+                    i++;
+                }
+                else if (CheckRightBracket(expression[i]))
+                {
+                    if (texas.Peek() is Operation)
+                        california.Push(texas.Pop());
+                    else if (CheckLeftBracket(texas.Peek()))
+                    {
+                        texas.Pop();
+                        i++;
+                    }
+                    else
+                        throw new Exception("Некорректная формула");
+                }
+                else if (expression[i] is Operation)
+                {
+                    switch ((expression[i] as Operation).Prior)
+                    {
+                        case (1):
+                            texas.Push(expression[i]);
+                            i++;
+                            break;
+                        case (2):
+                            if (!(texas.Peek() is Operation) || (texas.Peek() as Operation).Prior == 3)
+                            {
+                                texas.Push(expression[i]);
+                                i++;
+                            }
+                            else
+                                california.Push(texas.Pop());
+                            break;
+                        case (3):
+                            if (texas.Peek() is Operation)
+                                california.Push(texas.Pop());
+                            else
+                            {
+                                texas.Push(expression[i]);
+                                i++;
+                            }
+                            break;
+                    }
+                }
+                else if (CheckStartEnd(expression[i]))
+                {
+                    if (texas.Peek() is Operation)
+                        california.Push(texas.Pop());
+                    else if (CheckLeftBracket(texas.Peek()))                                                                           //ошибка
+                        throw new Exception("Левая скобка в конце выражения");
+                    else
+                        break;
+                }
+            }
+            return california.ToArray();
+        }
+        string GetRPNStr()
+        {
+            StringBuilder @string = new StringBuilder();
+            for (int i = Rpn.Count - 1; i >= 0; i--)
+            {
+                if (Rpn[i] is Operation)
+                    @string.Append((Rpn[i] as Operation).Name + " ");
+                else
+                    @string.Append(Rpn[i].ToString() + " ");
+            }
+            return @string.ToString();
+        }
+    }
+}
